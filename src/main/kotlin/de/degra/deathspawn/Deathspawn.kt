b550d6256365ab6b038ee30a255d9e720e1e50da
Oliver Nagel <rulestormer@yahoo.de>
2019-07-07 23:02:37 +0200

    Add singleton pattern

--- src/main/kotlin/de/degra/deathspawn/Deathspawn.kt
+++ src/main/kotlin/de/degra/deathspawn/Deathspawn.kt
@@ -4,8 +4,19 @@ import de.degra.deathspawn.listener.RespawnListener
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
-class Deathspawn : JavaPlugin() {
+class  Deathspawn : JavaPlugin() {
+
+    companion object {
+        private var inst: Deathspawn = null!!
+
+        fun getInst(): Deathspawn {
+            return inst
+        }
+    }
+
     override fun onEnable() {
+        inst = this;
+
         Bukkit.getPluginManager().registerEvents(RespawnListener(), this)
     }
 }

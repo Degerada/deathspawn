66b6f89c5a5c955c49ffb325b0616b1cd37f3b5a
Oliver Nagel <rulestormer@yahoo.de>
2019-07-08 03:31:45 +0200

    Change region to deathZoneId

--- src/main/kotlin/de/degra/deathspawn/models/DeathspawnEntry.kt
+++ src/main/kotlin/de/degra/deathspawn/models/DeathspawnEntry.kt
@@ -1,6 +1,5 @@
 package de.degra.deathspawn.models
 
-import com.sk89q.worldguard.protection.regions.ProtectedRegion
 import org.bukkit.Location
 
-data class DeathspawnEntry (val id: String, var zone: ProtectedRegion, var revLoc: Location)
+data class DeathspawnEntry (val id: Int, var deathZoneId: String, var revLoc: Location)

a2f704d009f7d27fd41eb4ab0c74e4f2ced76aa5
Oliver Nagel <rulestormer@yahoo.de>
2019-07-08 20:13:13 +0200

    Hook into World Guard flags

--- src/main/kotlin/de/degra/deathspawn/Deathspawn.kt
+++ src/main/kotlin/de/degra/deathspawn/Deathspawn.kt
@@ -1,10 +1,28 @@
 package de.degra.deathspawn
 
+import com.sk89q.worldguard.WorldGuard
+import com.sk89q.worldguard.protection.flags.LocationFlag
+import com.sk89q.worldguard.protection.flags.registry.FlagConflictException
 import de.degra.deathspawn.listener.RespawnListener
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
+import java.util.logging.Level
 
-class  Deathspawn : JavaPlugin() {
+class Deathspawn : JavaPlugin() {
+
+    companion object {
+        val DEATH_RESPAWN_FLAG = LocationFlag("death-respawn")
+    }
+
+    override fun onLoad() {
+        val registry = WorldGuard.getInstance().flagRegistry;
+        try {
+            // register our flag with the registry
+            registry.register(DEATH_RESPAWN_FLAG);
+        } catch (e: FlagConflictException) {
+            logger.log(Level.SEVERE, e.localizedMessage)
+        }
+    }
 
     override fun onEnable() {
         Bukkit.getPluginManager().registerEvents(RespawnListener(), this)
--- src/main/kotlin/de/degra/deathspawn/service/SpawnPointService.kt
+++ src/main/kotlin/de/degra/deathspawn/service/SpawnPointService.kt
@@ -2,72 +2,23 @@ package de.degra.deathspawn.service
 
 import com.sk89q.worldedit.bukkit.BukkitAdapter
 import com.sk89q.worldguard.WorldGuard
-import com.sk89q.worldguard.protection.regions.RegionContainer
-import de.degra.deathspawn.models.DeathspawnEntry
+import de.degra.deathspawn.Deathspawn.Companion.DEATH_RESPAWN_FLAG
 import org.bukkit.Location
 
 
 object SpawnPointService {
 
-    private val regionContainer: RegionContainer by lazy { WorldGuard.getInstance().platform.regionContainer }
-
-    var respawnZones: MutableList<DeathspawnEntry> = FilePersistService.readFromFile()
-
-    fun addRespawnZone(location: Location, zoneName: String): DeathspawnEntry {
-        var nextId = if (respawnZones.size > 0) {
-            respawnZones.maxBy { it.id }!!.id + 1
-        } else {
-            1
-        }
-        val deathspawnEntry = DeathspawnEntry(nextId, zoneName, location)
-        addRespawnZone(deathspawnEntry)
-        return deathspawnEntry
-    }
-
-    fun addRespawnZone(newEntry: DeathspawnEntry) {
-        respawnZones.add(newEntry)
-
-        refreshZones(respawnZones)
-    }
-
-    fun updateRespawnZone(updatedEntry: DeathspawnEntry) {
-        respawnZones.removeIf { it.deathZoneId == updatedEntry.deathZoneId }
-        respawnZones.add(updatedEntry)
-
-        refreshZones(respawnZones)
-    }
-
-    fun deleteRespawnZone(id: Int) {
-        respawnZones.removeIf { it.id == id }
-
-        refreshZones(respawnZones)
-    }
-
-    fun deleteRespawnZone(entry: DeathspawnEntry) {
-        respawnZones.remove(entry)
-
-        refreshZones(respawnZones)
-    }
-
-    fun refreshZones(respawnZones: MutableList<DeathspawnEntry>) {
-        FilePersistService.saveToFile(respawnZones)
-        this.respawnZones = FilePersistService.readFromFile()
-    }
-
-    /**
-     * Returns null if no appropriate revLocation was found
-     */
     fun getRespawnPos(deathLocation: Location): Location? {
+        val container = WorldGuard.getInstance().platform.regionContainer
+        val query = container.createQuery()
+        val set = query.getApplicableRegions(BukkitAdapter.adapt(deathLocation))
 
-        for (region in regionContainer[BukkitAdapter.adapt(deathLocation.world)]!!
-            .getApplicableRegions(BukkitAdapter.asBlockVector(deathLocation))) {
-            val zone = respawnZones
-                .stream()
-                .filter { it.deathZoneId == region.id }
-                .findAny()
-
-            if (zone.isPresent) {
-                return zone.get().revLoc
+        set.forEach { region ->
+            run {
+                val resFlag = region.getFlag(DEATH_RESPAWN_FLAG)
+                if (resFlag != null) {
+                    return BukkitAdapter.adapt(resFlag)
+                }
             }
         }
         return null

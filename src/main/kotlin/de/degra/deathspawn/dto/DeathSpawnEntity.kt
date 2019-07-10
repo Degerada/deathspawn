2a4fe5a9b719f6c5da5a0f708b576b4154aa2ec6
Oliver Nagel <rulestormer@yahoo.de>
2019-07-10 22:49:05 +0200

    Remove not needed classes

--- src/main/kotlin/de/degra/deathspawn/dto/DeathSpawnEntity.kt
+++
@@ -1,3 +0,0 @@
-package de.degra.deathspawn.dto
-
-data class DeathSpawnEntity(val id: Int, val zoneId: String, val position: PositionDTO)
--- src/main/kotlin/de/degra/deathspawn/dto/DeathSpawnEntityContainer.kt
+++
@@ -1,3 +0,0 @@
-package de.degra.deathspawn.dto
-
-data class DeathSpawnEntityContainer(var entries: MutableList<DeathSpawnEntity> = mutableListOf())

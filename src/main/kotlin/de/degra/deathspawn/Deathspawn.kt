e2171838385fe1ff13ba065d7678d814538e28ac
Oliver Nagel <rulestormer@yahoo.de>
2019-07-10 22:48:47 +0200

    Add sql and config file initialization to Deathspawn

--- src/main/kotlin/de/degra/deathspawn/Deathspawn.kt
+++ src/main/kotlin/de/degra/deathspawn/Deathspawn.kt
@@ -4,11 +4,18 @@ import com.sk89q.worldguard.WorldGuard
 import com.sk89q.worldguard.protection.flags.LocationFlag
 import com.sk89q.worldguard.protection.flags.registry.FlagConflictException
 import de.degra.deathspawn.listener.RespawnListener
+import de.degra.deathspawn.service.QueryService
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
 import java.util.logging.Level
 
+
 class Deathspawn : JavaPlugin() {
+
+    private var connection: Connection? = null
 
     companion object {
         val DEATH_RESPAWN_FLAG = LocationFlag("dspawn")
@@ -26,5 +33,28 @@ class Deathspawn : JavaPlugin() {
 
     override fun onEnable() {
         Bukkit.getPluginManager().registerEvents(RespawnListener(), this)
+        if (this.config["sql.enable"] == true) {
+            this.openConnection()
+        }
+    }
+
+    @Throws(SQLException::class, ClassNotFoundException::class)
+    fun openConnection() {
+        var connection: Connection? = null
+        if (connection != null && !connection!!.isClosed) {
+            return
+        }
+
+        synchronized(this) {
+            if (connection != null && !connection!!.isClosed) {
+                return
+            }
+            Class.forName("com.mysql.jdbc.Driver")
+            connection = DriverManager.getConnection(
+                ("jdbc:mysql://" + this.config["sql.address"] + ":" + this.config["sql.port"] + "/" + this.config["sql.database"]),
+                this.config["sql.username"].toString(), this.config["sql.password"].toString()
+            )
+            QueryService.connection = connection
+        }
     }
 }
